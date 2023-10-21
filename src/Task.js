8a1499cf4af9aa0caff6d99ab13b403230e5dbc2
hharpreetk <harpreetk2120@gmail.com>
2023-10-21 09:01:41 -0400

    refactor text editing and modification

--- src/Task.js
+++ src/Task.js
@@ -7,12 +7,24 @@ export default function Task({ task }) {
   const [editedText, setEditedText] = useState(task.text); // State to track edited text
 
   // Access dispatch function from context
-  const {dispatch} = useTasks();
+  const { dispatch } = useTasks();
+
+  // Function to handle saving changes when the "Save" button is clicked
+  const handleSave = () => {
+    // Check if the trimmed edited text is not empty
+    const trimmedEditedText = editedText.trim();
+    // Call the function to modify the task
+    if (trimmedEditedText) modifyTask(task.id, trimmedEditedText, task.done);
+    // Switch back to task text display
+    setEditing(false);
+  };
 
   // Function to modify an existing task (update text and done status)
   const modifyTask = (id, text, done) => {
-    // Dispatch an action to modify the task
-    dispatch({ type: "modify", id, text, done });
+    if (text !== task.text || done !== task.done) {
+      // Dispatch an action to modify the task only if changes are made
+      dispatch({ type: "modify", id, text, done });
+    }
   };
 
   // Function to delete a task
@@ -31,14 +43,9 @@ export default function Task({ task }) {
           value={editedText}
           maxLength={255}
           onChange={(e) => setEditedText(e.target.value)}
-          required
         />
         <button
-          onClick={() => {
-            setEditing(false);
-            const trimmedEditedText = editedText.trim();
-            if (trimmedEditedText) modifyTask(task.id, trimmedEditedText, task.done); // Save the edited text
-          }}
+          onClick={handleSave}
           disabled={!editedText.trim()} // Disable the  button when there's no text input
         >
           Save
@@ -54,6 +61,7 @@ export default function Task({ task }) {
       </>
     );
   }
+
   return (
     <>
       <input
