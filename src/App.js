b7cba3b666c467cea922bdef6fb0cf6acd568a31
hharpreetk <harpreetk2120@gmail.com>
2023-10-22 09:34:07 -0400

    add Navbar with dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import Navbar from "./Navbar";
 import TaskList from "./TaskList";
 import TaskInput from "./TaskInput";
 import { TasksProvider } from "./TasksContext";
+import { useState } from "react";
 
 export default function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div
-      className="flex h-screen w-full flex-1 flex-col overflow-hidden bg-slate-100 dark:bg-slate-900"
-      role="main"
-    >
-      <TasksProvider>
-        <TaskInput />
-        <TaskList />
-      </TasksProvider>
+    <div className={`App ${darkMode ? "dark" : ""}`}>
+      <div
+        className="flex h-screen w-full flex-1 flex-col overflow-hidden bg-slate-100 dark:bg-slate-900"
+        role="main"
+      >
+        <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        <TasksProvider>
+          <TaskInput />
+          <TaskList />
+        </TasksProvider>
+      </div>
     </div>
   );
 }
